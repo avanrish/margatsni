001ae70e4a56565619e49686be2fd120d6ebe362
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2022-02-06 12:57:03 +0100

    wip: chats - prepare icon & rename active/inactive icons

---
+++ src/pages/direct/inbox/[chat].tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Header from '../../../components/Header';
+
+export default function Chat() {
+  return (
+    <div>
+      <Head>
+        <title>A</title>
+      </Head>
+      <Header />
+      Chat
+    </div>
+  );
+}
---
+++ src/pages/direct/inbox/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Header from '../../../components/Header';
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>A</title>
+      </Head>
+      <Header />
+      Index
+    </div>
+  );
+}
