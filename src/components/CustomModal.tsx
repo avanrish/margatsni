2f2c8befaaad9376b7948bdd5b6b61f018e58e6a
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2022-02-20 16:43:59 +0100

    refactor: change how create post & success modal look

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -1,12 +1,14 @@
 import Modal from 'react-responsive-modal';
 
-export default function CustomModal({ children, open, onClose, ...rest }) {
+export default function CustomModal({ children, open, onClose, medium = false, ...rest }) {
   return (
     <Modal
       open={open}
       onClose={onClose}
       classNames={{
-        modal: 'flex flex-col w-full !max-w-xs rounded-lg !p-0 !text-center divide-y text-sm',
+        modal: `flex flex-col w-full rounded-lg !p-0 !text-center divide-y text-sm ${
+          medium ? '!max-w-md' : '!max-w-xs'
+        }`,
         modalContainer: 'overflow-y-hidden flex items-center justify-center',
       }}
       {...rest}
